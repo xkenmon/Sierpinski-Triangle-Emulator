76d22758fe2975efa3ffc7020a466c8a70f00441
xkenmon <xkenmon@gmail.com>
2021-12-05 19:16:20 +0800

    add slider widget

---
+++ README.md
@@ -0,0 +1,10 @@
+## Sierpinski Triangle Emulator
+
+A simple [Sierpiński triangle](https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle) Emulator, powerd by [Iced](https://github.com/iced-rs/iced).
+
+Left-click add fixed point, right-click remove fixed point.
+
+You can run with cargo:
+```
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::{fmt::Debug, time::Duration};
+use std::{fmt::Debug, ops::Not};
 
 use iced::{
-    canvas::{Cache, Frame, Geometry, Path, Program, Stroke},
-    executor, time, Application, Canvas, Command, Container, Length, Point, Settings, Size,
-    Subscription,
+    canvas::{event, Cache, Event, Path, Program, Stroke},
+    executor, slider, Application, Canvas, Color, Column, Command, Length, Point, Row, Settings,
+    Size, Slider, Text,
 };
+
 use rand::Rng;
 
 fn main() -> iced::Result {
@@ -16,23 +17,18 @@ fn main() -> iced::Result {
 
 #[derive(Debug)]
 struct SierpinskiEmulator {
-    fix_points: Vec<Point>,
-    random_points: Vec<Point>,
-    bound: Size<f32>,
-    cache: Cache,
-    version: usize,
-    duration: std::time::Duration,
+    graph: SierpinskiGraph,
+    max_iter_state: slider::State,
+    cur_iter_state: slider::State,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Message {
-    AddRandomPoint,
-    ReDraw,
-}
-
-#[derive(Debug)]
-pub enum TickError {
-    CommonError,
+    SetMaxIter(i32),
+    SetCurIter(i32),
+    DrawCurIter(i32),
+    AddFixPoint(Point),
+    RemoveFixPoint,
 }
 
 impl Application for SierpinskiEmulator {
@@ -43,24 +39,16 @@ impl Application for SierpinskiEmulator {
     type Flags = ();
 
     fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
-        let bound = Size::new(400_f32, 400_f32);
         let emulator = SierpinskiEmulator {
-            fix_points: vec![
-                Point::new(200_f32, 400_f32),
-                Point::new(0_f32, 0_f32),
-                Point::new(400_f32, 0_f32),
-            ],
-            random_points: vec![],
-            cache: Cache::new(),
-            bound,
-            version: 0,
-            duration: Duration::from_millis(50),
+            max_iter_state: slider::State::default(),
+            cur_iter_state: slider::State::default(),
+            graph: SierpinskiGraph::new(),
         };
         (emulator, Command::none())
     }
 
     fn title(&self) -> String {
-        "Sierpinski Emulator".to_string()
+        "Sierpinski Triangle Emulator".to_string()
     }
 
     fn update(
@@ -69,56 +57,151 @@ impl Application for SierpinskiEmulator {
         _clipboard: &mut iced::Clipboard,
     ) -> iced::Command<Self::Message> {
         match message {
-            Message::AddRandomPoint => {
-                self.version += 1;
-                let p = self.gen_rand_point();
-                self.random_points.push(p);
-                if self.version % 100 == 0 {
-                    self.cache.clear();
+            Message::SetMaxIter(max_iter) => {
+                self.graph.max_iter = max_iter;
+                while self.graph.random_points.len() < max_iter as usize {
+                    let p = self.graph.gen_rand_point();
+                    self.graph.random_points.push(p);
                 }
             }
-            Message::ReDraw => {
-                self.cache.clear();
+            Message::SetCurIter(cur_iter) => {
+                if cur_iter > self.graph.max_iter {
+                    self.graph.cur_iter = self.graph.max_iter;
+                } else {
+                    self.graph.cur_iter = cur_iter;
+                }
+            }
+            Message::AddFixPoint(point) => {
+                self.graph.fix_points.push(point);
+                self.graph.random_points.clear();
+                self.graph.max_iter = 0;
+                self.graph.cur_iter = 0;
+            }
+            Message::RemoveFixPoint => {
+                self.graph.fix_points.pop();
+                self.graph.random_points.clear();
+                self.graph.max_iter = 0;
+                self.graph.cur_iter = 0;
+            }
+            Message::DrawCurIter(cur_iter) => {
+                self.graph.cur_iter = cur_iter;
             }
         }
+        self.graph.redraw();
 
         Command::none()
     }
 
     fn view(&mut self) -> iced::Element<'_, Self::Message> {
-        let bound = self.bound;
-        let canvas = Canvas::new(self)
-            .width(Length::Units(bound.width as u16))
-            .height(Length::Units(bound.height as u16));
+        let bound = self.graph.bound;
+        let max_iter = self.graph.max_iter;
+        let cur_iter = self.graph.cur_iter;
+        let fix_point_is_empty = self.graph.fix_points.is_empty();
 
-        Container::new(canvas)
+        let mut content = Column::new()
             .width(Length::Fill)
-            .height(Length::Fill)
-            .padding(20)
-            .center_x()
-            .center_y()
-            .into()
-    }
-
-    fn subscription(&self) -> Subscription<Message> {
-        time::every(self.duration).map(|_| Message::AddRandomPoint)
+            .align_items(iced::Align::Center)
+            .push(
+                Canvas::new(&mut self.graph)
+                    .width(Length::Units(bound.width as u16))
+                    .height(Length::Units(bound.height as u16)),
+            );
+        if fix_point_is_empty.not() {
+            content = content
+                .push(
+                    Row::new()
+                        .padding(10)
+                        .spacing(20)
+                        .push(Text::new(format!("max iter: {:?}", max_iter)))
+                        .push(
+                            Slider::new(
+                                &mut self.max_iter_state,
+                                0..=10000,
+                                max_iter,
+                                Message::SetMaxIter,
+                            )
+                            .width(Length::Units(bound.width as u16)),
+                        ),
+                )
+                .push(
+                    Row::new()
+                        .padding(10)
+                        .spacing(20)
+                        .push(Text::new(format!("cur iter: {:?}", cur_iter)))
+                        .push(
+                            Slider::new(
+                                &mut self.cur_iter_state,
+                                0..=10000,
+                                cur_iter,
+                                Message::SetCurIter,
+                            )
+                            .width(Length::Units(bound.width as u16)),
+                        ),
+                );
+        }
+        content.into()
     }
 }
 
-impl Program<Message> for SierpinskiEmulator {
+#[derive(Debug)]
+struct SierpinskiGraph {
+    max_iter: i32,
+    cur_iter: i32,
+    fix_points: Vec<Point>,
+    random_points: Vec<Point>,
+    bound: Size<f32>,
+    cache: Cache,
+}
+
+impl Program<Message> for SierpinskiGraph {
+    fn update(
+        &mut self,
+        event: iced::canvas::Event,
+        bounds: iced::Rectangle,
+        cursor: iced::canvas::Cursor,
+    ) -> (iced::canvas::event::Status, Option<Message>) {
+        let cursor_position = if let Some(position) = cursor.position_in(&bounds) {
+            position
+        } else {
+            return (event::Status::Ignored, None);
+        };
+
+        match event {
+            Event::Mouse(mouse_event) => {
+                let message = match mouse_event {
+                    iced::mouse::Event::ButtonPressed(iced::mouse::Button::Left) => {
+                        Some(Message::AddFixPoint(cursor_position))
+                    }
+                    iced::mouse::Event::ButtonPressed(iced::mouse::Button::Right) => {
+                        Some(Message::RemoveFixPoint)
+                    }
+                    _ => None,
+                };
+                (event::Status::Captured, message)
+            }
+            _ => (event::Status::Ignored, None),
+        }
+    }
+
     fn draw(
         &self,
         bounds: iced::Rectangle,
         _cursor: iced::canvas::Cursor,
     ) -> Vec<iced::canvas::Geometry> {
         let geom = self.cache.draw(bounds.size(), |frame| {
-            self.random_points.iter().for_each(|p| {
-                let path = Path::rectangle(*p, Size::new(1_f32, 1_f32));
-                frame.stroke(&path, Stroke::default())
-            });
+            frame.stroke(
+                &Path::rectangle(Point::ORIGIN, frame.size()),
+                Stroke::default(),
+            );
+            self.random_points[0..self.cur_iter as usize]
+                .iter()
+                .for_each(|p| {
+                    let path = Path::rectangle(*p, Size::new(1_f32, 1_f32));
+                    frame.stroke(&path, Stroke::default())
+                });
             self.fix_points.iter().for_each(|p| {
-                let path = Path::rectangle(*p, Size::new(2_f32, 2_f32));
-                frame.stroke(&path, Stroke::default())
+                let path = Path::circle(*p, 5.0);
+                frame.fill(&path, Color::from_rgb8(0x12, 0x93, 0xD8));
             });
         });
 
@@ -126,7 +209,22 @@ impl Program<Message> for SierpinskiEmulator {
     }
 }
 
-impl SierpinskiEmulator {
+impl SierpinskiGraph {
+    fn new() -> SierpinskiGraph {
+        SierpinskiGraph {
+            max_iter: 0,
+            cur_iter: 0,
+            fix_points: vec![],
+            random_points: vec![],
+            bound: Size::new(600.0, 600.0),
+            cache: Cache::new(),
+        }
+    }
+
+    fn redraw(&mut self) {
+        self.cache.clear();
+    }
+
     fn gen_rand_point(&self) -> Point {
         let dest_point_idx = rand::thread_rng().gen_range(0..self.fix_points.len());
         let dest_point = self.fix_points[dest_point_idx];
